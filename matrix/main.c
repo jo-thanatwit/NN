77bc3d0ed527b7c8633f1c17f5df5282e56b3456
Thanatwit <jo42343@hotmail.com>
2019-03-10 21:11:11 +0700

    Initial Commit

---
+++ matrix/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  matrix
+//
+//  Created by Thanatwit  Angsarawanee on 10/3/2562 BE.
+//  Copyright © 2562 Thanatwit  Angsarawanee. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
